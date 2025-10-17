86bfd02cb97e67bb190252a6966e4288a05c9856
crhman <crahmanbahal@gmail.com>
2025-10-17 01:55:36 -0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRouts.js';
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/', userRoutes);
 
